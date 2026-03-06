cb8a82efacbc80bbefc760df60617cc2b5c9e762
PREM-KUMAR-1028 <spremkumar787@gmail.com>
2026-03-06 11:39:10 +0530

    Create Solution.cpp

---
+++ HackerRank/C++/For Loop/Solution.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    string w[]={"","one","two","three","four","five","six","seven","eight","nine"};
+    for(int i=a;i<=b;i++) 
+    {
+        if(i>=1 && i<=9)
+         {
+            cout<<w[i]<<endl;
+         } 
+        else if(i>9)
+         {
+            if(i%2 == 0) 
+            {
+                cout<<"even"<<endl;
+            } else 
+            {
+                cout<<"odd"<<endl;
+            }
+         }
+    }
+ return 0;
+}
+
