cbc3bbb934f65e694794adc5ec73508047d73376
PREM-KUMAR-1028 <spremkumar787@gmail.com>
2026-03-06 12:03:25 +0530

    Create Solution.cpp

---
+++ HackerRank/C++/StringStream/Solution.cpp
@@ -0,0 +1,29 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+vector<int> parseInts(string str)
+{
+    stringstream ss(str);
+    vector<int> result;
+    int temp_int;
+    char comma;
+    while (ss >> temp_int)
+    {
+        result.push_back(temp_int);
+        ss >> comma;
+    }
+    return result;
+}
+int main()
+{
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++)
+    {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
