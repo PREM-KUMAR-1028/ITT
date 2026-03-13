d73b064855c96d6cf8758e167db7fc852e542f6f
PREM-KUMAR-1028 <spremkumar787@gmail.com>
2026-03-13 11:41:42 +0530

    Create Answer.py

---
+++ HackerRank/C++/Python/Python If-Else/Answer.py
@@ -0,0 +1,12 @@
+def fun(n):
+    if n % 2 != 0:
+        print("Weird")
+    else:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
+a = int(input().strip())
+fun(a)
